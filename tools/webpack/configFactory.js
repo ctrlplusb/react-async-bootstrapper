a26f862dc4b70b923cfd6880d4492cd2067bb155
Sean Matheson <sean@ctrlplusb.com>
2017-03-17 22:13:54 +0800

    Updates to react-tree-walker@2.0.0-alpha.3

--- tools/webpack/configFactory.js
+++ tools/webpack/configFactory.js
@@ -63,13 +63,7 @@ function webpackConfigFactory({ target }) {
         {
           test: /\.js$/,
           loader: 'babel-loader',
-          include: [
-            resolvePath(appRootDir.get(), './src'),
-            resolvePath(appRootDir.get(), './node_modules/p-limit'),
-            resolvePath(appRootDir.get(), './node_modules/p-locate'),
-            resolvePath(appRootDir.get(), './node_modules/p-map-series'),
-            resolvePath(appRootDir.get(), './node_modules/p-reduce'),
-          ],
+          include: [resolvePath(appRootDir.get(), './src')],
         },
       ],
     },
