4284c3e545559c89d607f4f5b5501fe975ad2573
Sean Matheson <sean@ctrlplusb.com>
2017-04-06 12:13:07 +0100

    Allows an additional 'options' parameter to be passed into react-tree-walker.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import reactTreeWalker from 'react-tree-walker'
 
-export default function asyncBootstrapper(app) {
+export default function asyncBootstrapper(app, options) {
   const visitor = (element, instance) => {
     if (instance && typeof instance.asyncBootstrap === 'function') {
       return instance.asyncBootstrap()
@@ -8,5 +8,5 @@ export default function asyncBootstrapper(app) {
     return true
   }
 
-  return reactTreeWalker(app, visitor, {})
+  return reactTreeWalker(app, visitor, {}, options)
 }
